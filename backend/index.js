9ce1eb7f4be693d29cf1ea27ed30713ce8129479
mrsardorr <snazirjonov09@gmail.com>
2023-09-05 12:26:25 +0500

    backend node i guess

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,41 @@
-const express = require("express")
-const cors = require("cors")
-const { default: axios } = require("axios")
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
-const app = express()
-app.use(express.json())
-app.use(cors({origin:true}))
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
 
-app.post("/auth",async(req,res)=>{
-    const {username} = req.body;
-    try {
-        const resp = await axios.put(
-            "http://api.chatengine.io/users",
-            {username:username,secret:username,firstName:username},
-            {headers:{"private-key":"fc481e8f-c46f-46ae-99d8-074687f396f0"}}
-        )
-        return res.status(resp.status).json(r.data)
-    } catch (e) {
-        return res.status(e.response.status).json(e.response.data)
-    }
-    return res.json({username:username,secret:"sha256..."})
+const CHAT_ENGINE_PROJECT_ID = "7e05968b-9d63-4a13-a0e8-59f45e5ed9f2";
+const CHAT_ENGINE_PRIVATE_KEY = "fc481e8f-c46f-46ae-99d8-074687f396f0";
+
+app.post("/signup", async (req, res) => {
+  const { username, secret, email, first_name, last_name } = req.body;
+  try {
+    const r = await axios.post(
+      "https://api.chatengine.io/users/",
+      { username, secret, email, first_name, last_name },
+      { headers: { "Private-Key": CHAT_ENGINE_PRIVATE_KEY } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
-app.listen(1234)
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        "Project-ID": CHAT_ENGINE_PROJECT_ID,
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+app.listen(3001);
